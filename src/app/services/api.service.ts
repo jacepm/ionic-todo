14f7d623b8d1ff3922f70277f554930b543f84a2
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-01 03:51:06 +0800

    FEAT: Add get function in api.service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment as env } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class ApiService {
-  constructor() {}
+  API_URL = env.API_URL;
+
+  constructor(private http: HttpClient) {}
+
+  get(uri: string): any {
+    return this.http.get(this.API_URL + uri);
+  }
 }
