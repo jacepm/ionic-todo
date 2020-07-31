50129a8295e0ff18f0ec0cbd341c27fdc7d37be9
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-01 04:24:03 +0800

    FEAT: Add getTodos function in home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../services/api.service";
+import { ITodos } from "../interface/todos.interface";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  todos: ITodos[] = [];
 
-  constructor() {}
+  constructor(private api: ApiService) {}
 
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    this.api.get(`todos`).subscribe((todos: ITodos[]) => (this.todos = todos));
+  }
 }
